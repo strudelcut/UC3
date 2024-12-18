058ef4739968eaddce5eba5aac023ab86286cc53
strudelcut <113107454+strudelcut@users.noreply.github.com>
2024-12-18 19:52:53 -0300

    gold

--- Projeto/projeto_django/car/urls.py
+++ Projeto/projeto_django/car/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.car),
+    path('', views.car, name='car'),
 ]
--- Projeto/projeto_django/tabela/urls.py
+++ Projeto/projeto_django/tabela/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tabela, ),
+    path('', views.tabela, name='tabela'),
 ]

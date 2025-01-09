a9707e8bd2309e3f65b3ed2f4dbd2213abf34e07
strudelcut <113107454+strudelcut@users.noreply.github.com>
2025-01-08 21:21:20 -0300

    Aula 11

--- Aula 07/projeto/cadastro/urls.py
+++ Aula 07/projeto/cadastro/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.cadastro, name='cadastro'),
     path('gravar/', views.gravar, name='gravar'),
+    path('mostrar/', views.mostrar, name='mostrar'),
+    path('apagar/<int:id>', views.apagar, name='apagar'),
+    path('editar/<int:id>', views.editar, name='editar'),
+    path('atualizar/<int:id>', views.atualizar, name='atualizar'),
 ]
--- Aula 07/projeto/cadastro/views.py
+++ Aula 07/projeto/cadastro/views.py
@@ -2,15 +2,53 @@ from django.shortcuts import render
 from .models import Pessoa
 
 # Create your views here.
-contexto = {'title': 'Cadastro - Salão de Beleza Visual da Moda'}
+
+# Carregar o objeto, classe Pessoa
+pessoas = Pessoa.objects.all()
+
+contexto = {
+    'title': 'Cadastro - Salão de Beleza Visual da Moda',
+    'pessoas': pessoas
+    }
+
+# Carregar o template cadastro.html
 def cadastro(request):
     return render(request, 'cadastro/index.html', contexto)
 
+# Salvar os dados de contato
 def gravar(request):
-    # Salvar os dados de contato
     nova_pessoa = Pessoa()
     nova_pessoa.nome = request.POST.get('nome')
     nova_pessoa.idade = request.POST.get('idade')
     nova_pessoa.email = request.POST.get('email')
     nova_pessoa.save()
     return cadastro(request)
+
+# Mostrar os dados de contato e carregar o template mostrar.html
+def mostrar(request):
+    pessoas = Pessoa.objects.all()
+    contexto = {
+    'title': 'Cadastro - Salão de Beleza Visual da Moda',
+    'pessoas': pessoas
+    }
+    return render(request, 'cadastro/mostrar.html', contexto)
+
+# Editar os dados de contato
+def editar(request, id):
+    pessoa = Pessoa.objects.get(id_pessoa=id)
+    return render(request, 'cadastro/editar.html', {'pessoa': pessoa})
+
+# Atualizar os dados de contato
+def atualizar(request, id):
+    pessoa = Pessoa.objects.get(id_pessoa=id)
+    pessoa.nome = request.POST.get('nome')
+    pessoa.idade = request.POST.get('idade')
+    pessoa.email = request.POST.get('email')
+    pessoa.save()
+    return mostrar(request)
+
+# Apagar os dados de contato
+def apagar(request, id):
+    pessoa = Pessoa.objects.get(id_pessoa=id)
+    pessoa.delete()
+    return mostrar(request)

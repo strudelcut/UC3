f453bc5fe23befcc0d9c9ae324f445e32218f263
strudelcut <113107454+strudelcut@users.noreply.github.com>
2025-01-16 09:58:42 -0300

    Modal e newsletter adicionados v2

--- Projeto/projeto_django/homev2/urls.py
+++ Projeto/projeto_django/homev2/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.homev2, name='homev2'),
     path('soquetes/', views.soquetes, name='soquetes'),
+    path('newsletter/', views.newsletterV2, name='newsletter'),
 ]
--- Projeto/projeto_django/homev2/views.py
+++ Projeto/projeto_django/homev2/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-a = {'title': 'Home V2 - Blog'}
-b = {'title': 'Soquetes'}
+# a = {'title': 'Home V2 - Blog'}
+# b = {'title': 'Soquetes'}
 
 def homev2(request):
-    return render(request, 'homev2/index.html', a)
+    return render(request, 'homev2/index.html', {'title': 'Home V2 - Blog'})
+
 def soquetes(request):
-    return render(request, 'homev2/soquetes.html', b)
+    return render(request, 'homev2/soquetes.html', {'title': 'Soquetes'})
+
+def newsletterV2(request):
+    return render(request, 'homev2/newsletter.html', {'title': 'Newsletter'})

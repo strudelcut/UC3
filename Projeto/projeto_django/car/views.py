589850b8ac7673ca093b60e3210c92a165541402
strudelcut <113107454+strudelcut@users.noreply.github.com>
2025-01-23 12:08:20 -0300

    Card pelas imagens do BD

--- Projeto/projeto_django/car/views.py
+++ Projeto/projeto_django/car/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from home.serializers import SerializarImagem
+from home.models import Imagem
 
 # Create your views here.
+
+class ImagemViewSet(viewsets.ModelViewSet):
+    queryset = Imagem.objects.all()
+    serializer_class = SerializarImagem
 
 a = {'title': 'Carrossel - Processadores'}
 
 def car(request):
+    
+    a = {'title': 'Carrossel - Processadores', 'imagens' : Imagem.objects.all()}
     return render(request, 'car/index.html', a)
 

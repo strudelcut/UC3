09fbd28a95f490554fe5e6e21a795d72b45fb186
strudelcut <113107454+strudelcut@users.noreply.github.com>
2025-01-26 00:41:33 -0300

    cadastrar editar v2

--- Projeto/projeto_django/homev2/urls.py
+++ Projeto/projeto_django/homev2/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('', views.homev2, name='homev2'),
     path('soquetes/', views.soquetes, name='soquetes'),
     path('newsletter/', views.newsletterV2, name='newsletterv2'),
+    path('cadastro/', views.cadastroV2, name='cadastrov2'),
+    path('editar/<int:id>', views.editarV2, name='editarv2'),
+    path('mostrar/', views.listaV2, name='mostrarv2'),
+    path('salvar/', views.add_cadastroV2, name='salvarv2'),
+    path('atualizar/<int:id>', views.editar_cadastroV2, name='atualizarv2'),
+    path('apagar/<int:id>', views.apagarV2, name='apagarv2'),
 ]
--- Projeto/projeto_django/homev2/views.py
+++ Projeto/projeto_django/homev2/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from rest_framework import viewsets
+from home.serializers import SerializarImagem
+from home.models import Remetente, Usuario, Imagem
 
 # Create your views here.
 # a = {'title': 'Home V2 - Blog'}
@@ -12,3 +15,62 @@ def soquetes(request):
 
 def newsletterV2(request):
     return render(request, 'homev2/newsletter.html', {'title': 'Newsletter'})
+
+def cadastroV2(request):
+    return render(request, 'homev2/cadastro.html', {'title': 'Cadastro'})
+
+def editarV2(request, id):
+    usuario = Usuario.objects.get(id=id)
+    if request.method == 'POST':
+        usuario.nome = request.POST['nome']
+        usuario.sobrenome = request.POST['sobrenome']
+        usuario.sexo = request.POST['sexo']
+        usuario.cep = request.POST['cep']
+        
+    return render(request, 'homev2/editar.html', {'edit': usuario})
+
+def listaV2(request):
+    usuarios = Usuario.objects.all()
+    c = {
+        'title': 'Lista',
+        'usuarios': usuarios
+    }
+
+    return render(request, 'homev2/listadb.html', c)
+
+def add_cadastroV2(request):
+    if request.method == 'POST':
+        novo_cadastro = Usuario()
+        novo_cadastro.nome = request.POST['nome']
+        novo_cadastro.sobrenome = request.POST['sobrenome']
+        novo_cadastro.sexo = request.POST['sexo']
+        novo_cadastro.cep = request.POST['cep']
+        novo_cadastro.rua = request.POST['rua']
+        novo_cadastro.numero = request.POST['numero']
+        novo_cadastro.complemento = request.POST['complemento']
+        novo_cadastro.bairro = request.POST['bairro']
+        novo_cadastro.cidade = request.POST['cidade']
+        novo_cadastro.uf = request.POST['uf']
+        novo_cadastro.save()
+        return redirect(request.META.get('HTTP_REFERER', '/'))
+    return render(request, cadastroV2(request))
+
+def editar_cadastroV2(request, id):
+    novo_cadastro = Usuario.objects.get(id=id)
+    novo_cadastro.nome = request.POST['nome']
+    novo_cadastro.sobrenome = request.POST['sobrenome']
+    novo_cadastro.sexo = request.POST['sexo']
+    novo_cadastro.cep = request.POST['cep']
+    novo_cadastro.rua = request.POST['rua']
+    novo_cadastro.numero = request.POST['numero']
+    novo_cadastro.complemento = request.POST['complemento']
+    novo_cadastro.bairro = request.POST['bairro']
+    novo_cadastro.cidade = request.POST['cidade']
+    novo_cadastro.uf = request.POST['uf']
+    novo_cadastro.save()
+    return redirect(reverse('mostrarv2'))
+
+def apagarV2(request, id):
+    cadastro = Usuario.objects.get(id=id)
+    cadastro.delete()
+    return redirect(reverse('mostrarv2'))
